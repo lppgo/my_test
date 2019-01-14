4f10383a96ec732489bd4143966628edeb2d3ce0
李平平 <37990195+lppgo@users.noreply.github.com>
2019-01-14 15:01:50 +0800

    update  nsq消息队列 producer

--- 010NSQ/pub/main.go
+++ 010NSQ/pub/main.go
@@ -1,35 +1,62 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"strconv"
+	"log"
 
-	"github.com/nsqio/go-nsq"
+	"github.com/go-nsq"
 )
 
 var (
+	producer *nsq.Producer
 	//nsqd的地址，使用了tcp监听的port
 	tcpNsqdAddr = "127.0.0.1:4150"
+	topic       = "Insert"
 )
+
+//发送消息结构体
+type ProduceMessage struct {
+	Name  string `json:"name"`
+	Index int    `json:"index"`
+	//参数类型，不同参数对应不同的处理方法
+	Type int `json:"type"` //0:偶数，1：奇数
+}
 
 //producer
 func main() {
-	//initConfig
-	config := nsq.NewConfig()
-	for i := 0; i < 10; i++ {
-		//创建100个生产者Producer
-		tPro, err := nsq.NewProducer(tcpNsqdAddr, config)
-		if err != nil {
-			fmt.Println("New producer failed :" + err.Error())
-		}
-		//主题
-		topic := "Insert"
+	//初始化配置
+	config := initConfig()
+	//创建Producer
+	var createProducerErr error
+	producer, createProducerErr = nsq.NewProducer(tcpNsqdAddr, config)
+	if createProducerErr != nil {
+		fmt.Println("New producer failed :" + createProducerErr.Error())
+	}
+	for i := 0; i < 100; i++ {
 		//主题内容
-		tContent := "New data " + strconv.Itoa(i)
+		tContent := &ProduceMessage{
+			Name:  "lucas_nsq_是一个实时消息队列",
+			Index: i,
+			Type:  i % 2, //取余数
+		}
+		//将数据编码
+		body, err := json.Marshal(tContent)
+		if err != nil {
+			fmt.Println("json_marshal_err:" + err.Error())
+		}
 		//发布消息
-		err = tPro.Publish(topic, []byte(tContent))
+		err = producer.Publish(topic, body)
 		if err != nil {
 			fmt.Println("Publish failed :" + err.Error())
 		}
 	}
+	log.Println("All producer publish end !")
+	select {}
+}
+
+// initConfig 初始化配置
+func initConfig() *nsq.Config {
+	config := nsq.NewConfig()
+	return config
 }
