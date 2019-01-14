b7a149df8f6394183adcbcb36727c1c5365b6ee1
李平平 <37990195+lppgo@users.noreply.github.com>
2019-01-14 15:02:41 +0800

    update nsq消息队列 consumer

--- 010NSQ/sub/main.go
+++ 010NSQ/sub/main.go
@@ -1,56 +1,99 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"sync"
-	"time"
 
 	"github.com/nsqio/go-nsq"
 )
 
 var (
 	//nsqd的地址，使用tcp监听端口
-	tcpNsqAddr = "127.0.0.1:4150"
+	nsqLookupds = "127.0.0.1:4150"
+	topic       = "Insert" //主题
+	channel     = "my_nsq_channel_1"
+	gocount     = 10 //该参数指示为消息处理生成的goroutine的数量
 )
 
-//结构体实现HandleMessage接口方法
-type NsqHandler struct {
-	msgCount     int64  //消息数目
-	nsqHandlerID string //标识ID
+//消费消息结构体
+type ConsumeMessage struct {
+	Name  string `json:"name"`
+	Index int    `json:"index"`
+	//参数类型，不同参数对应不同的处理方法
+	Type int `json:"type"` //0:偶数，1：奇数
 }
 
-//实现HandleMessage方法
-func (s *NsqHandler) HandleMessage(message *nsq.Message) error {
-	//每次收到一条消息+1
-	s.msgCount++
-	//打印输出信息和ID
-	fmt.Println(s.msgCount, s.nsqHandlerID)
-	//打印消息的一些基本信息
-	fmt.Printf("msg.TimesTamp=%v,msg.nsqaddr=%s,msg.body=%v\n", time.Unix(0, message.Timestamp).Format("2006-01-02 15:04:05"), message.NSQDAddress, string(message.Body))
-	return nil
+const (
+	Odd  = 1 //类型是奇数
+	Even = 0 //类型是偶数
+)
+
+//根据类型进行消息分发处理器
+type Handler struct {
 }
 
 func main() {
 	//初始化配置
 	config := nsq.NewConfig()
 	//创建消费者
-	consumer, err := nsq.NewConsumer("Insert", "channel1", config)
+	consumer, err := nsq.NewConsumer(topic, channel, config)
 	if err != nil {
 		fmt.Println("NewConsumer failed :" + err.Error())
 	}
-	//添加处理回调
-	handler := NsqHandler{
-		nsqHandlerID: "1",
-	}
-	consumer.AddHandler(&handler)
+	//消息分发处理
+	consumer.AddConcurrentHandlers(&Handler{}, gocount)
+	// consumer.AddHandler(&Handler{})
 	//连接对应的nsqd
-	err = consumer.ConnectToNSQD(tcpNsqAddr)
+	//ConnectToNSQLookupds将多个nsqlookupd地址添加到此comsumer实例的列表中。
+	//如果添加第一个地址，它将启动一个HTTP请求来发现配置主题的nsqd生成器。
+	err = consumer.ConnectToNSQD(nsqLookupds)
 	if err != nil {
 		fmt.Println("ConnectToNSQD failed : " + err.Error())
+		panic(err)
 	}
+	fmt.Println("end ... ")
+	select {}
+}
 
-	//只是为了不结束此进程，这里没有意义
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	wg.Wait()
+// 实现HandleMessage方法,进行消息分发处理
+func (s *Handler) HandleMessage(message *nsq.Message) error {
+	msg := ConsumeMessage{}
+	err := json.Unmarshal(message.Body, &msg)
+	if err != nil {
+		fmt.Println("解码message发生错误：" + err.Error())
+		return err
+	}
+	if 0 == msg.Type {
+		OddErr := HandleOddMessage(msg)
+		if OddErr != nil {
+			fmt.Println("OddErr :" + OddErr.Error())
+			return OddErr
+		}
+	} else if 1 == msg.Type {
+		EvenErr := HandleEvenMessage(msg)
+		if EvenErr != nil {
+			fmt.Println("EvenErr :" + EvenErr.Error())
+			return EvenErr
+		}
+	}
+	message.Finish()
+	return nil
+}
+
+var oddCount, evenCount int
+
+//奇数处理
+func HandleOddMessage(msg ConsumeMessage) error {
+	oddCount++
+	fmt.Printf("message 是奇数，index是%d\n", oddCount)
+	fmt.Printf("%+v\n", msg)
+	return nil
+}
+
+//偶数处理
+func HandleEvenMessage(msg ConsumeMessage) error {
+	oddCount++
+	fmt.Printf("message 是偶数，index是%d\n", oddCount)
+	fmt.Printf("%+v\n", msg)
+	return nil
 }
