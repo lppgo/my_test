75732fea6c9b2888a925e7b68ad84d5a6cd9d72b
李平平 <37990195+lppgo@users.noreply.github.com>
2019-05-07 18:02:11 +0800

    Create server.go

---
+++ 009_tcp_rpc/server/server.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"net/rpc"
+	"os"
+)
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+type Arith int
+
+func (t *Arith) Multiply(args *Args, reply *int) error {
+	*reply = args.A * args.B
+	return nil
+}
+
+func (t *Arith) Divide(args *Args, quo *Quotient) error {
+	if args.B == 0 {
+		return errors.New("divide by zero")
+	}
+	quo.Quo = args.A / args.B
+	quo.Rem = args.A % args.B
+	return nil
+}
+func init() {
+	fmt.Println("基于 TCP 协议实现的RPC，服务端的代码如下")
+}
+func main() {
+	arith := new(Arith)
+	//  注册rpc服务
+	rpc.Register(arith)
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", ":1235")
+	checkError(err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		rpc.ServeConn(conn)
+	}
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
