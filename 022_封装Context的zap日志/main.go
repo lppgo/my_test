38aca2f355fb241900e4185c1937b0059e0212e1
李平平 <37990195+lppgo@users.noreply.github.com>
2019-05-17 18:19:03 +0800

    新更新

--- 022_封装Context的zap日志/main.go
+++ 022_封装Context的zap日志/main.go
@@ -1,45 +1,30 @@
-package main
-
+package main 
 import (
-	"context"
-
-	"git.yeeuu.com/yeeuu/my_test/099_temp_test/logger"
-)
-
-var (
-	mlog logger.MLogger
-)
-
-func init() {
-	path := "./stdout.log"
-	logger.InitLog(path, "debug")
-	mlog = logger.Logger
+    "github.com/labstack/echo/v4"
+    "fmt"
+    "git.yeeuu.com/yeeuu/my_test/temptest/mlog"
+    "git.yeeuu.com/yeeuu/my_test/temptest/middleware/ctxlog"
+ )
+func init(){
+    mlog.InitLog("./test.log", "debug")
 }
 
-func main() {
-	//
-
-	ctx := context.WithValue(context.Background(), "ClientIP", "127.0.0.1")
-	ctx = context.WithValue(ctx, "RequestID", "fcvuosqav8912")
-	ctx = context.WithValue(ctx, "DentifyID", 1000)
-	ctx = context.WithValue(ctx, "ServersID", "mountain_go")
-	// mlog := logger.MLogger
-	str := "hello ccc"
-	// logger.Logger.Info(ctx context.Context, args ...interface{})
-	// mlog := logger.Logger
-	mlog.Info(ctx, str)
-	mlog.Infof("template", ctx, str)
-	mlog.Debug(ctx, "debug一个信息")
-	mlog.Debugf("模板", ctx, "debug模板信息")
-
-	// mlog.Debugw("消息", ctx, "k1", "v1")
-	// mlog.Infow("消息", ctx, "k1", "v1")
-	// mlog.Warnw("消息", ctx, "k1", "v1")
-	// mlog.DPanicw("消息", ctx, "k1", "v1")
-	mlog.DPanicw("消息", ctx, "k1", "v1")
-	// mlog.Debugw("消息", "Context", ctx, "k1", "v1")
-	// mlog.Warn(ctx context.Context, args ...interface{})
-	// mlog.Warnf(template string, ctx context.Context, args ...interface{})
-	// mlog.Info(ctx context.Context, args ...interface{})
-
+func main(){
+    echo:= echo.New()
+    echo.Use(ctxlog.HandleCtxParams())
+    echo.GET("/", indexController)
+    echo.Start(":1323")
 }
+
+
+func indexController(c echo.Context) error{
+	fmt.Println("--------------mlog----start-------------------")
+
+	mlog.Logger.Infow(c.Request().Context(), "测试日志", "err_key_1", "value_1")
+	mlog.Logger.Info(c.Request().Context(),  "err_key_1", "value_1")
+	mlog.Logger.Errorw(c.Request().Context(), "测试日志", "err_key_1", "value_1")
+	mlog.Logger.Error(c.Request().Context(),  "err_key_1", "value_1")
+    fmt.Println("--------------mlog------end-----------------")
+    return nil
+}
+
