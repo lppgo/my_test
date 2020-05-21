05e10d578065b3aeae8fb59e34f318b6043f333e
李平平 <37990195+lppgo@users.noreply.github.com>
2020-05-21 17:54:44 +0800

    Update util.go

--- 000_utils/util.go
+++ 000_utils/util.go
@@ -2,10 +2,11 @@ package util
 
 import (
 	"unsafe"
+	"reflect"
 )
 
 //https://www.cnblogs.com/shuiyuejiangnan/p/9707066.html
-
+//--------------------------string和[]byte高效互转--------------------------------------
 // string高效转换为[]byte
 func Str2Bytes(s string) []byte {
 	x := (*[2]uintptr)(unsafe.Pointer(&s))
@@ -16,4 +17,26 @@ func Str2Bytes(s string) []byte {
 // []byte高效转换为string
 func Bytes2Str(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
+}
+
+//-----------------------------结构体和[]byte互转----------------------------------------
+type MyStruct struct {
+	A int
+	B int
+}
+
+var sizeOfMyStruct = int(unsafe.Sizeof(MyStruct{}))
+
+func MyStructToBytes(s *MyStruct) []byte {
+	var x reflect.SliceHeader
+	x.Len = sizeOfMyStruct
+	x.Cap = sizeOfMyStruct
+	x.Data = uintptr(unsafe.Pointer(s))
+	return *(*[]byte)(unsafe.Pointer(&x))
+}
+
+func BytesToMyStruct(b []byte) *MyStruct {
+	return (*MyStruct)(unsafe.Pointer(
+		(*reflect.SliceHeader)(unsafe.Pointer(&b)).Data,
+	))
 }
