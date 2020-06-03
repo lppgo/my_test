67453e8eb06799133290b33429814905caa0d42d
李平平 <37990195+lppgo@users.noreply.github.com>
2020-06-03 11:23:28 +0800

    add IsEmpty()判断是否为空的函数

--- 000_utils/util.go
+++ 000_utils/util.go
@@ -6,7 +6,7 @@ import (
 )
 
 //https://www.cnblogs.com/shuiyuejiangnan/p/9707066.html
-//--------------------------string和[]byte高效互转--------------------------------------
+//--------------------------string和[]byte高效互转------------------------------------------1-------------------------------------
 // string高效转换为[]byte
 func Str2Bytes(s string) []byte {
 	x := (*[2]uintptr)(unsafe.Pointer(&s))
@@ -19,7 +19,7 @@ func Bytes2Str(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
 }
 
-//-----------------------------结构体和[]byte互转----------------------------------------
+//-----------------------------结构体和[]byte互转-------------------------------------------2------------------------------------
 type MyStruct struct {
 	A int
 	B int
@@ -39,4 +39,63 @@ func BytesToMyStruct(b []byte) *MyStruct {
 	return (*MyStruct)(unsafe.Pointer(
 		(*reflect.SliceHeader)(unsafe.Pointer(&b)).Data,
 	))
+}
+// -----------------------------IsEmpty()判断给的值是否为空----------------------------------------3--------------------------------
+// IsEmpty checks whether given <value> empty.
+// It returns true if <value> is in: 0, nil, false, "", len(slice/map/chan) == 0.Or else it returns true.
+func IsEmpty(value interface{}) bool {
+	if value == nil {
+		return true
+	}
+	switch value := value.(type) {
+	case int:
+		return value == 0
+	case int8:
+		return value == 0
+	case int16:
+		return value == 0
+	case int32:
+		return value == 0
+	case int64:
+		return value == 0
+	case uint:
+		return value == 0
+	case uint8:
+		return value == 0
+	case uint16:
+		return value == 0
+	case uint32:
+		return value == 0
+	case uint64:
+		return value == 0
+	case float32:
+		return value == 0
+	case float64:
+		return value == 0
+	case bool:
+		return value == false
+	case string:
+		return value == ""
+	case []byte:
+		return len(value) == 0
+	default:
+		// Finally using reflect.
+		rv := reflect.ValueOf(value)
+		switch rv.Kind() {
+		case reflect.Chan,
+			reflect.Map,
+			reflect.Slice,
+			reflect.Array:
+			return rv.Len() == 0
+
+		case reflect.Func,
+			reflect.Ptr,
+			reflect.Interface,
+			reflect.UnsafePointer:
+			if rv.IsNil() {
+				return true
+			}
+		}
+	}
+	return false
 }
