d3deccf8b952f07c5d00e7e3288537ee33eb35e0
李平平 <37990195+lppgo@users.noreply.github.com>
2020-06-13 14:14:54 +0800

    Update jwt.go

--- 001_jwt中间件/jwt.go
+++ 001_jwt中间件/jwt.go
@@ -115,6 +115,7 @@ func (j *JWT) RefreshToken(tokenString string) (string, error) {
 }
 
 func (j *JWT) CreateToken(claims CustomClaims) (string, error) {
+	claims.StandardClaims.ExpiresAt = time.Now().Add(24 * time.Hour).Unix()
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(j.SigningKey)
 }
